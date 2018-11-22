e431f5245b07fa5c34a441556cfd37dc63a043ee
Kostas Stergiannis <stekostas@gmail.com>
2018-11-22 16:21:07 +0100

    Add main plugin file

---
+++ woocommerce-seven-senders-order-tracking.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:  WooCommerce Seven Senders Order Tracking
+ * Plugin URI:   https://github.com/hypeventures/woocommerce-seven-senders-order-tracking
+ * Description:  Interacts with the Seven Senders API to provide order tracking.
+ * Version:      0.0.1
+ * Author:       Kostas Stergiannis <kostas@invinciblebrands.com>
+ * Author URI:   https://www.invinciblebrands.com/
+ * License:      GPL3
+ * License URI:  https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:  woocommerce-seven-senders-order-tracking
+ * Domain Path:  /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
