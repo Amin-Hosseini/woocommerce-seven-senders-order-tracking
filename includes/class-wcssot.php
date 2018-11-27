11ccc72b2d186af4e35529ae8926ef0b7e929e26
Kostas Stergiannis <stekostas@gmail.com>
2018-11-27 15:40:07 +0100

    Add the settings page in the admin menu

--- includes/class-wcssot.php
+++ includes/class-wcssot.php
@@ -33,6 +33,43 @@ final class WCSSOT {
 	 * @return void
 	 */
 	private function initialise_hooks() {
+		if ( is_admin() ) {
+			add_action( 'admin_menu', [ $this, 'add_admin_menu' ] );
+		}
+	}
 
+	/**
+	 * Adds the administration menu page.
+	 *
+	 * @since 0.0.1
+	 * @return void
+	 */
+	public function add_admin_menu() {
+		add_menu_page(
+			'Seven Senders Order Tracking',
+			'Order Tracking',
+			'manage_options',
+			'wcssot',
+			[ $this, 'render_admin_page' ],
+			plugin_dir_url( WCSSOT_PLUGIN_FILE ) . 'admin/images/icon_wcssot.png',
+			100
+		);
+	}
+
+	/**
+	 * Renders the admin settings page.
+     *
+     * @since 0.0.1
+     * @return void
+	 */
+	public function render_admin_page() {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+		?>
+        <div class="wrap">
+            <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+        </div>
+		<?php
 	}
 }
