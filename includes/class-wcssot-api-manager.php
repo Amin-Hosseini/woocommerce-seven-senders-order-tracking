4c38a5d26b866b39f1f52f5acb1a726c781be386
Kostas Stergiannis <stekostas@gmail.com>
2018-12-03 10:04:42 +0100

    Declare the API manager class

---
+++ includes/class-wcssot-api-manager.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Contains the Seven Senders API manager class.
+ *
+ * Copyright (C) 2018 Invincible Brands GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @package WCSSOT
+ * @subpackage Includes
+ * @since 0.2.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * The Seven Senders API manager class responsible for all operations with the gateway.
+ *
+ * @since 0.2.0
+ *
+ * @class WCSSOT_API_Manager
+ */
+class WCSSOT_API_Manager {
+	/**
+	 * WCSSOT_API_Manager constructor.
+	 *
+	 * @since 0.2.0
+	 */
+	public function __construct() {
+		/** @todo Initialise properties. */
+	}
+}
