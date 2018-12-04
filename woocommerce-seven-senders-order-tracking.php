8cb1154be45c1f20281732ad10b12f6e5b372088
Kostas Stergiannis <stekostas@gmail.com>
2018-12-04 15:19:47 +0100

    Increment version to next minor

--- woocommerce-seven-senders-order-tracking.php
+++ woocommerce-seven-senders-order-tracking.php
@@ -3,7 +3,7 @@
  * Plugin Name:  WooCommerce Seven Senders Order Tracking
  * Plugin URI:   https://github.com/hypeventures/woocommerce-seven-senders-order-tracking
  * Description:  Interacts with the Seven Senders API to provide order tracking.
- * Version:      0.2.0
+ * Version:      0.3.0
  * Author:       Kostas Stergiannis <kostas@invinciblebrands.com>
  * Author URI:   https://www.invinciblebrands.com/
  * License:      GPL3
