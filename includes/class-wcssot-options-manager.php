9bb8637a19eedc15dc7d88ac2b6ef685b73c25c4
Kostas Stergiannis <stekostas@gmail.com>
2019-01-02 10:34:39 +0100

    Declare the new options manager class

---
+++ includes/class-wcssot-options-manager.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The plugin options manager class.
+ *
+ * Copyright (C) 2018 Invincible Brands GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @package WCSSOT
+ * @subpackage Includes
+ * @since 1.2.0
+ */
+
+namespace WCSSOT;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * The plugin options manager class.
+ *
+ * @since 1.2.0
+ *
+ * @class WCSSOT_Options_Manager
+ */
+class WCSSOT_Options_Manager {
+
+}
