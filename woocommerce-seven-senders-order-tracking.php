bb543a404b17b03e19b995f3e0e84e40a835ff81
Achim Galeski <achim.galeski@gmail.com>
2019-08-23 16:49:34 +0200

    changed author to Invincible Brands

--- woocommerce-seven-senders-order-tracking.php
+++ woocommerce-seven-senders-order-tracking.php
@@ -4,7 +4,7 @@
  * Plugin URI:   https://github.com/hypeventures/woocommerce-seven-senders-order-tracking
  * Description:  Interacts with the Seven Senders API to provide order tracking functionality to your WooCommerce shop.
  * Version:      2.0.2
- * Author:       Kostas Stergiannis <kostas@invinciblebrands.com>
+ * Author:       Invincible Brands <dev@invinciblebrands.com>
  * Author URI:   https://www.invinciblebrands.com/
  * License:      GPL3
  * License URI:  https://www.gnu.org/licenses/gpl-3.0.html
